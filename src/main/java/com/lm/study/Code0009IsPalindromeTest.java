7a77a61ec2ea97d3af5156d553b389a52d26077f
limin <1095020285@qq.com>
2021-02-27 22:21:45 +0800

    leetcode-0009

--- src/main/java/com/lm/study/Code0009IsPalindromeTest.java
+++ src/main/java/com/lm/study/Code0009IsPalindromeTest.java
@@ -8,26 +8,21 @@ import org.junit.Test;
  * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
  * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数
  *
+ * level 2
  * @author limin
  * @date 2021/2/21
  */
 public class Code0009IsPalindromeTest {
-    /**
-     * 将数字作为字符串进行处理
-     * */
     public boolean isPalindrome(int x) {
-        String inputNum = String.valueOf(x);
-        char[] inputChar = inputNum.toCharArray();
-        int i = 0;
-        int j = inputChar.length - 1;
-        while (i < j) {
-            if (inputChar[i] != inputChar[j]) {
-                return false;
-            }
-            i++;
-            j--;
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        return true;
+        int result = 0;
+        while (result < x) {
+            result = 10 * result + x % 10;
+            x /= 10;
+        }
+        return result == x || result / 10 == x;
     }
 
     @Test
@@ -37,5 +32,6 @@ public class Code0009IsPalindromeTest {
         Assert.assertFalse(isPalindromeTest.isPalindrome(-121));
         Assert.assertFalse(isPalindromeTest.isPalindrome(10));
         Assert.assertFalse(isPalindromeTest.isPalindrome(-101));
+        Assert.assertFalse(isPalindromeTest.isPalindrome(21120));
     }
 }
